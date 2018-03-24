eef208f223191a8f9cb6cfc051e1f2ed21d20a0f
admin <wach.grzegorz@gazeta.pl>
2018-03-24 17:03:57 +0100

    First exercises on events

--- src/a.js
+++ src/a.js
@@ -2,10 +2,37 @@ console.log('I am a.js!');
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded. Ready to go!");
+
+counterEverySeconds();
+
 });
 
-// A.1 - Stworz licznik ktory nalicza numery co 1 sekunde i wstawia liczbe to html
-// A.2 - Stworz minutnik ktory posiada strukture 00:00 - inkrementuje sekundy, pokazuje ile uplynelo minut i sekund
+// A.1 - Stworz licznik ktory nalicza numery co 1 sekunde
+// i wstawia liczbe to html
+
+const counterEverySeconds = () => {
+  let counter = 0;
+  const span = document.querySelector('#counter');
+  span.innerText = counter;
+  const oneMore = setInterval(() => {
+    counter++;
+    span.innerText = counter;
+  }, 1000);
+}
+
+
+
+// A.2 - Stworz minutnik ktory posiada strukture 
+// 00:00 - inkrementuje sekundy, pokazuje ile uplynelo 
+//minut i sekund
+
+const a2 = () => {
+  
+}
+
+
+
+
 // A.3 - Stworz funkcje ktora na wejsciu dostaje co ile minutnik ma sie inkrementowac - po wywolaniu
 // ma dodac do strony kolejny minutnik ktory inkrementuje zgodnie z zadanym argumentem
 // A.4 - Dodaj mozliwość ustawienia maksymalnej liczby minutnika po ktorej minutnik ma sie zatrzymac
--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,14 @@
 console.log('I am playground.js!');
 
+const doItInSecond = () => {
+  setTimeout(() => {
+    console.log('After second');
+  }, 1000);
+  console.log('I am called before');
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded. Ready to go!");
+
+  doItInSecond();
 });
